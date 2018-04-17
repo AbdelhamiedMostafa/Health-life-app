e1d9058f3b9deca701b72f487994d11bc0e99aa0
farah <farah.mahmoud7080@gmail.com>
2018-04-17 02:04:38 +0200

    edit in feedback

--- 4healthyLife_App/_src/app/src/main/java/com/healthyLife/android/FeedbackSendActivity.java
+++ 4healthyLife_App/_src/app/src/main/java/com/healthyLife/android/FeedbackSendActivity.java
@@ -1,13 +1,30 @@
 package com.healthyLife.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FeedbackSendActivity extends AppCompatActivity {
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback_send);
+        home = (Button) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeBtn();
+            }
+        });
+
+    }
+
+    public void homeBtn() {
+        Intent intent = new Intent(FeedbackSendActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
